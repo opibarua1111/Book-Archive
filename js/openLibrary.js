189e858c48d435c7d269fd816ea2e5550576976a
WF <opibarua1111@gmail.com>
2021-09-02 13:41:13 +0600

    some change

--- js/openLibrary.js
+++ js/openLibrary.js
@@ -19,7 +19,7 @@ const searchbooks = async () => {
     {
         document.getElementById('alert-message').style.display = 'none';
         //load data
-        const url = ` http://openlibrary.org/search.json?q=${searchText}`;
+        const url = ` https://openlibrary.org/search.json?q=${searchText}`;
         try{
             const res = await fetch(url);
             const data = await res.json();
